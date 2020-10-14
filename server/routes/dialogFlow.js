10fc636cf2b357132c59b1ea47b08f3bf275cac2
unknown <kalyansingh277@gmail.com>
2020-10-14 19:08:25 +0530

    Added Bots and some changes

--- server/routes/dialogFlow.js
+++ server/routes/dialogFlow.js
@@ -4,7 +4,6 @@ const router = express.Router();
 
 const DialogFlowCtrl = require('../controllers/dialogFlow');
 
-router.get('', DialogFlowCtrl.getDialog);
 router.post(
   "/event", DialogFlowCtrl.eventDialog
 );
